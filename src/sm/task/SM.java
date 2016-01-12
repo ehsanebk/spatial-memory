793d6d35e3f3da194b5cdbd1c95d1dca1979085b
Ehsan Khosroshahi <eb452@drexel.edu>
2016-01-11 20:56:40 -0500

    no message

--- src/sm/task/SM.java
+++ src/sm/task/SM.java
@@ -3,15 +3,19 @@ package sm.task;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Rectangle;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import java.util.Vector;
 
+import javax.sql.rowset.spi.TransactionalWriter;
+
 import actr.model.Event;
 import actr.model.Symbol;
 import actr.task.Result;
+import actr.task.Statistics;
 import actr.task.Task;
 import actr.task.TaskButton;
 import actr.task.TaskCross;
@@ -59,42 +63,57 @@ public class SM extends Task {
 	private int trial = 0;
 	private List<VisualObject> visualObjects = new Vector<VisualObject>();
 	private int numberOfObjects = 0;
+	private int currentOrder;
+	private double currentDelay;
+	private double lastTime = 0;
 	private TaskButton doneButton;
-	
+	private Random random = new Random();	
 	private VisualObject pickedObject = null;
 	
-	//private int askedPlane = 0;
-	private int[] responses = new int[NUMBER_OF_TRIALS];
-	private double[] errors = new double[NUMBER_OF_TRIALS];
-
-	private Random random = new Random();
+	private Values modelMeanDistnaces = new Values();
+	private Values modelMeanRT = new Values();
 	
+	double [][] modelData = {           
+			//setsize+order 500:mean-distance  mean-rt  1000:mean-distance  mean-rt  1500:mean-distance  mean-rt
+			{        21,        0,             0,            0,             0,            0,             0       },
+			{        22,        0,             0,            0,             0,            0,             0       },
+			{        31,        0,             0,            0,             0,            0,             0       },
+			{        32,        0,             0,            0,             0,            0,             0       },
+			{        33,        0,             0,            0,             0,            0,             0       },
+			{        41,        0,             0,            0,             0,            0,             0       },
+			{        42,        0,             0,            0,             0,            0,             0       },
+			{        43,        0,             0,            0,             0,            0,             0       },
+			{        44,        0,             0,            0,             0,            0,             0       },
+			{        51,        0,             0,            0,             0,            0,             0       },
+			{        52,        0,             0,            0,             0,            0,             0       },
+			{        53,        0,             0,            0,             0,            0,             0       },
+			{        54,        0,             0,            0,             0,            0,             0       },
+			{        55,        0,             0,            0,             0,            0,             0       }};
+                     
+
 	public SM() {
 		super();
 		setBackground(Color.gray);
+
 		doneButton = new TaskButton("DONE", DISPLAY_WIDTH-100, DISPLAY_HEIGHT - 50, 80,40 ){
 			@Override
 			public void doClick() {
-				switch (phase) {
-				case scan:
-					addEvent(new Event(DELAY_TIME[random.nextInt(3)], "task", "update") {
+				if (phase == Phase.scan){
+					currentDelay =DELAY_TIME[random.nextInt(3)];
+					// making the changes after DELAY_TIME
+					addEvent(new Event( SM.this.getModel().getTime()+ currentDelay, "task", "update") {
 						@Override
 						public void action() {
 							recallPhase();
 						}
 					});
-					
-					break;
-				case recall:
-					scanPhase();
-					break;
-				default:
-					break;
+
 				}
+				else
+					scanPhase();
 			}
 		};
 		add(doneButton);
-		add(new TaskCross(X_CENTER, Y_CENTER, 5));
 	}
 
 	@Override
@@ -106,11 +125,13 @@ public class SM extends Task {
 				scanPhase();
 			}
 		});
+		add(new TaskCross(X_CENTER, Y_CENTER, 5));
 	}
 
 	private void scanPhase() {
 		trial++;
 		if (trial > NUMBER_OF_TRIALS){
+			computeModelAverage();
 			stopModel();	
 		}
 		if (visualObjects.size() > 0)
@@ -155,7 +176,7 @@ public class SM extends Task {
 	}
 
 	private void recallPhase() {
-		
+		currentOrder = 1;
 		phase = Phase.recall;
 		for (int i = 0; i < visualObjects.size(); i++) {
 			visualObjects.get(i).moveTo(new Point(100*(i+1), 750) );
@@ -165,23 +186,12 @@ public class SM extends Task {
 		processDisplayNoClear();
 		getModel().getDeclarative().get(Symbol.get("goal")).set(Symbol.get("isa"),
 				Symbol.get("recall"));
-		System.out.println(getModel().getDeclarative().get(Symbol.get("goal")).toString());
+		//System.out.println(getModel().getDeclarative().get(Symbol.get("goal")).toString());
+		repaint();
+		processDisplay();
 		
 	}
 	
-//	private Plane getNearestPlane(double x, double y) {
-//		Plane nearest = null;
-//		double nearestDistance = 0;
-//		for (Plane plane : planes) {
-//			Point fp = plane.getLocation();
-//			double fd = fp.distance(x, y);
-//			if (nearest == null || fd < nearestDistance) {
-//				nearest = plane;
-//				nearestDistance = fd;
-//			}
-//		}
-//		return nearest;
-//	}
 
 	@Override
 	public void clickMouse() {
@@ -202,6 +212,7 @@ public class SM extends Task {
 				}
 			}
 			else if ((component instanceof VisualObject) && component.isVisible() && mouseY>700){
+				lastTime = getModel().getTime();
 				VisualObject object = (VisualObject) component;
 				Rectangle bounds = component.getBounds();
 				if (bounds.contains(mousePoint)) {
@@ -216,65 +227,129 @@ public class SM extends Task {
 		
 		if (pickedObject != null && phase == Phase.recall && mouseY < 700){
 			pickedObject.moveTo(mousePoint);
+			double RT = getModel().getTime() - lastTime;
+			allocateDistanceAndTimeForCurrentTria(pickedObject, RT ,numberOfObjects, currentOrder ,currentDelay);
 			repaint();
 			processDisplay();
 			processDisplayNoClear();
 			pickedObject = null;
+			currentOrder++;
 		}
 			
 	}
-	
+
 	private void stopModel() {
+	
+		// ordering the values in a list for getting correlation at the end 
+		for (int i = 0; i < modelData.length; i++) {
+			modelMeanDistnaces.add(modelData[i][1]);
+			modelMeanRT.add(modelData[i][2]);
+			modelMeanDistnaces.add(modelData[i][3]);
+			modelMeanRT.add(modelData[i][4]);
+			modelMeanDistnaces.add(modelData[i][5]);
+			modelMeanRT.add(modelData[i][6]);
+		}
+		
 		getModel().stop();
+	}
+
+	// run through the model data and sum up the distance order and response times to calculate the mean
+	private void allocateDistanceAndTimeForCurrentTria(VisualObject v , double responseTime, int numberOfObjects, int currentOrder , double currentDelay){
+		int setsize_order = numberOfObjects*10 + currentOrder;
+		for (int i = 0; i < modelData.length; i++) {
+			if (modelData[i][0] == setsize_order){
+				System.out.println(setsize_order);
+				 modelData[i][(int)currentDelay*4] += v.distanceFromOriginalPosition();
+				 modelData[i][(int)currentDelay*4 +1] += v.distanceFromOriginalPosition();
+			}	
+		}
 		
 	}
+	private void computeModelAverage(){
+		for (int i = 0; i < modelData.length; i++) {
+			for (int j = 1; j < modelData[i].length; j++) {
+				modelData[i][j] /= modelData[i][j] / trial;
+			} 	
+		}
+	}
+
+	
+
+	double [][] subjectData = {		
+//	   setsize order 500:mean-distance  mean-rt  1000:mean-distance  mean-rt  1500:mean-distance  mean-rt
+			{21,         48.98669,      2.118309,     51.06345,      1.977565,     49.39542,      2.108340},
+			{22,         57.17774,      2.060967,     60.71604,      2.083069,     65.27009,      2.057485},
+			{31,         52.59191,      2.089555,     57.68851,      2.120293,     59.14785,      2.114943},
+			{32,         79.52583,      2.066912,     72.39516,      2.193382,     78.24223,      2.189131},
+			{33,         91.71579,      1.975042,     83.36576,      2.035882,     86.20471,      2.061694},
+			{41,         66.33435,      2.174735,     67.27975,      2.259349,     69.61219,      2.284546},
+			{42,         79.94063,      2.094857,     84.32421,      2.191237,     84.53771,      2.189087},
+			{43,         106.47135,     2.069533,     01.27530,      2.153021,     108.70950,     2.206206},
+			{44,         106.77691,     1.916349,     10.63078,      1.945411,     113.19957,     1.999764},
+			{51,         74.11933,      2.496001,     78.10549,      2.433900,     76.17092,      2.408664},
+			{52,         91.23742,      2.225441,     98.99052,      2.285018,     103.93411,     2.357762},
+			{53,         120.28720,     2.194071,     19.91568,      2.166547,     127.75890,     2.311127},
+			{54,         133.92358,     2.168754,     37.39534,      2.092915,     136.47923,     2.113358},
+			{55,         131.44671,     1.899707,     30.99772,      1.934896,     135.33160,     1.884930}};
 
 	@Override
 	public Result analyze(Task[] tasks, boolean output) {
-//		try {
-//			Values allResponses = new Values();
-//			Values allErrors = new Values();
-//			Values[] responseValues = new Values[NUMBER_OF_TRIALS];
-//			Values[] errorValues = new Values[NUMBER_OF_TRIALS];
-//			for (int i = 0; i < NUMBER_OF_TRIALS; i++) {
-//				responseValues[i] = new Values();
-//				errorValues[i] = new Values();
-//			}
-//			for (Task taskCast : tasks) {
-//				SM task = (SM) taskCast;
-//				for (int i = 0; i < NUMBER_OF_TRIALS; i++) {
-//					allResponses.add(task.responses[i]);
-//					responseValues[i].add(task.responses[i]);
-//					if (task.responses[i] > 0) {
-//						allErrors.add(task.errors[i]);
-//						errorValues[i].add(task.errors[i]);
-//					}
-//				}
-//			}
-//
-//			DecimalFormat df1 = new DecimalFormat("#.0");
-//			DecimalFormat df3 = new DecimalFormat("#.000");
-//
-//			getModel().output("\n=========  Results  ===========\n");
-//
-//			getModel().output("Overall Correctness: " + df3.format(allResponses.mean()));
-//			getModel().output("Overall Location Error: " + df1.format(allErrors.mean()));
-//
-//			getModel().output("\nCorrectness by Trial:");
-//			String s = "";
-//			for (int i = 0; i < NUMBER_OF_TRIALS; i++)
-//				s += df3.format(responseValues[i].mean()) + "\t";
-//			getModel().output(s.trim());
-//
-//			getModel().output("\nLocation Error by Trial:");
-//			s = "";
-//			for (int i = 0; i < NUMBER_OF_TRIALS; i++)
-//				s += df1.format(errorValues[i].mean()) + "\t";
-//			getModel().output(s.trim());
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//
+		try {
+			
+			
+			Values humanMeanDistances = new Values();
+			Values humanMeanRT = new Values();
+			
+			for (int i = 0; i < subjectData.length; i++) {
+
+				humanMeanDistances.add(subjectData[i][1]);
+				humanMeanRT.add(subjectData[i][2]);
+				humanMeanDistances.add(subjectData[i][3]);
+				humanMeanRT.add(subjectData[i][4]);
+				humanMeanDistances.add(subjectData[i][5]);
+				humanMeanRT.add(subjectData[i][6]);
+			}
+			
+			
+			Values allModelMeanDistances = new Values();
+			Values allModelMeanRT = new Values();
+			
+			int numberOfExperiments= 0;
+			for (Task taskCast : tasks) {
+				SM task = (SM) taskCast;
+				allModelMeanDistances.add(task.modelMeanDistnaces);
+				allModelMeanRT.add(task.modelMeanRT);
+				numberOfExperiments++;
+			}
+			
+			allModelMeanDistances.divide(numberOfExperiments);
+			allModelMeanRT.divide(numberOfExperiments);
+
+			DecimalFormat df1 = new DecimalFormat("#.0");
+			DecimalFormat df3 = new DecimalFormat("#.000");
+
+			getModel().output("\n=========  Results  ===========\n");
+
+			if (output) {
+				double rDistance = humanMeanDistances.correlation(allModelMeanDistances);
+				double rRT = humanMeanRT.correlation(allModelMeanRT);
+				getModel().output("\n=====\n");
+				
+				
+				getModel().output("Correlation Value for Distances = " + String.format("%.2f", rDistance));
+				getModel().output("Correlation Value for RTs       = " + String.format("%.2f", rRT));
+				
+			}
+
+			
+			getModel().output("Overall Location Error: " + df1.format(allModelMeanDistances.mean()));
+			getModel().output("Overall Responces     : " + df1.format(allModelMeanRT.mean()));
+
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		return new Result();
 	}
 }
---
+++ src/sm/task/Values.java
@@ -0,0 +1,141 @@
+package sm.task;
+
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+import actr.task.Statistics;
+
+public class Values {
+	private ArrayList<Double> v;
+
+	public Values() {
+		v = new ArrayList<Double>();
+	}
+
+	public void add(double d) {
+		v.add(d);
+	}
+
+	public void add(Values vals2) {
+		for (int i = 0; i < vals2.size(); i++)
+			v.add(vals2.get(i));
+	}
+
+	public double correlation(Values vals2) {
+		double[] v1 = new double[v.size()];
+		double[] v2 = new double[vals2.size()];
+		if (v1.length == v2.length){
+			for (int i = 0; i < v2.length; i++) {
+				v1[i] = v.get(i);
+				v2[i] = vals2.get(i);
+			}
+			return Statistics.correlation(v1, v2);
+		
+		}
+		else{
+			return 0;
+		}
+	}
+	public void divide(double d) {
+		for (int i = 0; i < v.size(); i++)
+			v.set(i, v.get(i)/d);
+	}
+	public double get(int i) {
+		return v.get(i);
+	}
+
+	public void removeLast() {
+		if (v.size() > 0)
+			v.remove(v.size() - 1);
+	}
+
+	public int size() {
+		return v.size();
+	}
+
+	public double min() {
+		if (v.size() == 0)
+			return 0;
+		double min = v.get(0);
+		for (int i = 1; i < v.size(); i++)
+			if (v.get(i) < min)
+				min = v.get(i);
+		return min;
+	}
+
+	public double max() {
+		if (v.size() == 0)
+			return 0;
+		double max = v.get(0);
+		for (int i = 1; i < v.size(); i++)
+			if (v.get(i) > max)
+				max = v.get(i);
+		return max;
+	}
+
+	public double mean() {
+		if (v.size() == 0)
+			return 0;
+		double sum = 0;
+		for (int i = 0; i < v.size(); i++)
+			sum += v.get(i);
+		return sum / (1.0 * v.size());
+	}
+
+	public double average() {
+		return mean();
+	}
+
+	public double stddev() {
+		if (v.size() < 2)
+			return 0;
+		double mean = mean();
+		double sum = 0;
+		for (int i = 0; i < v.size(); i++)
+			sum += Math.pow(v.get(i) - mean, 2);
+		return Math.sqrt(sum / (1.0 * (v.size() - 1)));
+	}
+
+	public double stderr() {
+		if (v.size() < 2)
+			return 0;
+		return stddev() / Math.sqrt(1.0 * v.size());
+	}
+
+	public double confint() {
+		return 1.96 * stderr();
+	}
+
+	public double rmse(double expected) {
+		if (v.size() == 0)
+			return 0;
+		double sum = 0;
+		for (int i = 0; i < v.size(); i++)
+			sum += Math.pow(v.get(i) - expected, 2);
+		return Math.sqrt(sum / (1.0 * v.size()));
+	}
+
+	public double rmse() {
+		return rmse(0);
+	}
+
+	public String toString(DecimalFormat df) {
+		if (v.size() == 0)
+			return "";
+		String s = "";
+		s += df.format(v.get(0));
+		for (int i = 1; i < v.size(); i++)
+			s += "\t" + df.format(v.get(i));
+		return s;
+	}
+
+	private final DecimalFormat DEFAULT_FORMAT = new DecimalFormat("#.000");
+
+	@Override
+	public String toString() {
+		return toString(DEFAULT_FORMAT);
+	}
+}
